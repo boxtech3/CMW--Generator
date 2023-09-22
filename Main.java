3d290c26a89fde7c9803d08efbd6fdddf484eb17
boxtech3 <137317596+boxtech3@users.noreply.github.com>
2023-09-22 15:36:56 -0600

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,12 @@
+//Date: Sept.22, 2023
+//Author: Boxtech3 (Tristan)
+//Description: This program is designed to effectively generate and store CMW#s (and the associated item names) for all Items in CMW
+import java.util.scanner;
+import Item.java;
+
+
+class Main {
+  public static void main(String[] args) {
+    System.out.println("Welcome to the CMW# Generator");
+  }
+}
